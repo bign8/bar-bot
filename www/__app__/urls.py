47213621ed0418413fd7dbf6fbcd7d54f08053c8
Nate Woods <big.nate.w@gmail.com>
2015-01-10 14:00:56 -0700

    Custom format login page

--- www/__app__/urls.py
+++ www/__app__/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^', include('www.urls', namespace='www')),
 )
